dc7e9b90ad3d697822dd86053315a0820197196b
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2021-09-17 01:11:18 -0300

    Initial commit

---
+++ perimeter-area.py
@@ -0,0 +1,4 @@
+largQuadrado = float( input( "Digite o valor correspondente ao lado de um quadrado: " ) )
+perimeter = largQuadrado*4
+area = largQuadrado**2
+print( "perímetro: ", perimeter, " - ", "area: ", area )
