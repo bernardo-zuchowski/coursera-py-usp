518ffba6567ba73b85a643fcc2b3892524b2643a
bernardo-zuchowski <ber.s.zuc@gmail.com>
2021-09-20 13:36:06 -0300

    lista de exercícios 2

---
+++ 2-ex (1).py
@@ -0,0 +1,6 @@
+par_ou_impar = int(input("Digite um número inteiro para saber se é par ou ímpar: "))
+
+if (par_ou_impar%2) == 0 :
+    print( "par" )
+else:
+    print( "ímpar" )
---
+++ 2-ex (3).py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número para saber se ele é divisível por 3: "))
+
+if (num%5) == 0:
+    print("Buzz")
+else:
+    print(num)
