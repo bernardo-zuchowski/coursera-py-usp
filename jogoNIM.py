e7820e0a34303540eccedfb010b844033f714b1d
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2021-09-24 00:35:52 -0300

    week 6

--- jogoNIM.py
+++ jogoNIM.py
@@ -1,19 +1,9 @@
-#def computador_escolhe_jogada(n, m)
+def computador_escolhe_jogada(n, m)
 
-#def usuario_escolhe_jogada(n, m)
 
-cpu = computador_escolhe_jogada(n, m)
-user = usuario_escolhe_jogada(n, m)
-defnm = def_nm(n, m)
+def usuario_escolhe_jogada(n, m)
 
-def main(x):
-    x = 0
-    while x == 1 or x == 2:
-        x = int(input("Bem-vindo ao jogo do NIM! Escolha: /n/n 1 - para jogar uma partida isolada /n 2 - para jogar um campeonato"))
-        if x == 1:
-            return partida()
-        else: 
-            return campeonato()
+
 
 def def_nm(n, m):
     n = int(input("Quantas peças?"))
@@ -23,16 +13,36 @@ def def_nm(n, m):
     else:
         return campeonato
 
-def partida():
-    print("Você escolheu uma partida!")
-    return def_nm
+
+
+cpu = computador_escolhe_jogada(n, m)
+user = usuario_escolhe_jogada(n, m)
+defnm = def_nm(n, m)
+
+
+
+def main(x):
+    x = 0
+    while x == 1 or x == 2:
+        x = int(input("Bem-vindo ao jogo do NIM! Escolha: /n/n 1 - para jogar uma partida isolada /n 2 - para jogar um campeonato"))
+        if x == 1:
+            print("Você escolheu uma partida!")
+            return defnm
+        else: 
+            print("Você escolheu uma partida!")
+            return defnm
+
+
+
+def partida(n, m):
     if n * m + 1:
         print("Você começa!")
-        return user
+        while win != True:
+            return user
     else:
         print("Computador começa!")
         return cpu
     
 def campeonato():
-    print("Você escolheu uma partida!")
-    return def_nm
+    if main(2):
+        
